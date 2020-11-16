c337e18816e1328dbd1b81d00fb7fd025ccf8d39
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2020-11-16 16:05:53 +0800

    Lesson 5 code added

---
+++ LessonCode/Lesson5/App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text, Button ,View } from 'react-native';
+
+class App extends Component{
+  componentDidMount() {
+    console.log('Component DID MOUNT!')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Component DID UPDATE!')
+  }
+
+  // create a dummy state for demo
+  state = {
+    energy: -200
+  }
+
+  // sample event handler for button
+  buttonPress = () => {
+    this.setState({
+      energy: -500
+    });
+  }
+
+  render(){
+    // return JSX for UI
+    return(
+      <View>
+        <Text style={{ fontSize: 60}}>
+          Hello new app inside subfolder: {this.state.energy}
+        </Text>
+        <Button onPress={this.buttonPress} title="Increase" />
+      </View>
+    );
+  }
+}
+
+export default App;
