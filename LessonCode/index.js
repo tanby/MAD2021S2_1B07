b4a7b97fae822c4016a9f8d23abeb2a768018d0e
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2021-02-01 17:16:31 +0800

    generated APK

--- LessonCode/index.js
+++ LessonCode/index.js
@@ -6,8 +6,8 @@ import {AppRegistry} from 'react-native';
 //import App from './Lesson5/App';
 //import App from './Practical07/App';
 //import App from './Lesson8/App';
-//import App from './Lesson11/App';
-import App from './Firebase/App';
+import App from './Lesson11/App';
+//import App from './Firebase/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
